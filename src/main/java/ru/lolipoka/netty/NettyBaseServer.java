3d51c9f90a6c458732b28c6abcf20dcf04733d30
Alexey Adamov <booqueworme@gmail.com>
2021-05-26 08:43:03 +0300

    + Netty server example

---
+++ src/main/java/ru/lolipoka/netty/NettyBaseServer.java
@@ -0,0 +1,53 @@
+package ru.lolipoka.netty;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.Channel;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.handler.codec.string.StringDecoder;
+import io.netty.handler.codec.string.StringEncoder;
+import ru.lolipoka.netty.handlers.ByteBufInputHandler;
+import ru.lolipoka.netty.handlers.ChatMessageHandler;
+import ru.lolipoka.netty.handlers.OutputHandler;
+
+public class NettyBaseServer {
+
+    public NettyBaseServer() {
+        EventLoopGroup auth = new NioEventLoopGroup(1);
+        EventLoopGroup worker = new NioEventLoopGroup();
+
+        try {
+            ServerBootstrap bootstrap = new ServerBootstrap();
+            bootstrap.group(auth, worker)
+                    .channel(NioServerSocketChannel.class)
+                    .childHandler(new ChannelInitializer<Channel>() {
+                        @Override
+                        protected void initChannel(Channel ch) {
+                            ch.pipeline().addLast(
+                                    new StringEncoder(),
+                                    new StringDecoder(),
+                                    new ByteBufInputHandler(),
+                                    new OutputHandler(),
+                                    new ChatMessageHandler()
+                            );
+                        }
+                    });
+            ChannelFuture future = bootstrap.bind(4000).sync();
+            System.out.println("Server started");
+            future.channel().closeFuture().sync();
+            System.out.println("Server closed");
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } finally {
+            auth.shutdownGracefully();
+            worker.shutdownGracefully();
+        }
+    }
+
+    public static void main(String[] args) {
+        new NettyBaseServer();
+    }
+}
